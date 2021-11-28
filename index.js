cae95be8f6bc57b3ce03a9b66506ebdb807a236d
gcsaperas-konodrac <90689726+gcsaperas-konodrac@users.noreply.github.com>
2021-11-28 13:17:13 +0100

    # Add root route # Add Procfile

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send('Welcome to HTML Measure Conversion API!')
+})
 app.use("/", convertRouter);
 
 /**
